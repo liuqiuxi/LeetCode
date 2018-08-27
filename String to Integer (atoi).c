b0ca08be18e5131f6c25e6b6c448b03fe2dbaa38
liuqiuxi <35030517+liuqiuxi@users.noreply.github.com>
2018-08-27 11:17:21 +0800

    Add files via upload

---
+++ String to Integer (atoi).c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+#define INT_MAX  2147483648
+#define INT_MIN -2147483647-1
+
+int myAtoi(char* str) {
+	int flag = 1, result = 0, dig;
+	while (*str == ' ')
+	{
+		str++;
+	}
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+		{
+			flag = -1;
+		}
+		str++;
+	}
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+		{
+			return flag * result;
+		}
+		dig = *str - '0';
+		if (flag == 1 && result * 10.0 + dig > INT_MAX)
+		{
+			return INT_MAX;
+		}
+		else if (flag == -1 && -1 * result * 10.0 - dig < INT_MIN)
+		{
+			return INT_MIN;
+		}
+		result = result * 10 + dig;
+		str++;
+	}
+	result = flag * result;
+	return result;
+}
+
+
+int main()
+{
+	char *s = "-91283472332";
+	int i = myAtoi(s);
+	printf("%d\n", i);
+	return 0;
+}
