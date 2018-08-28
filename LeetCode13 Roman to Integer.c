3d665157f27b75763e753005f6d56e0b485f02b7
liuqiuxi <35030517+liuqiuxi@users.noreply.github.com>
2018-08-28 14:10:42 +0800

    Add files via upload

---
+++ LeetCode13 Roman to Integer.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int getInt(char s)
+{
+	switch (s)
+	{
+	case 'I':
+		return 1;
+	case 'V':
+		return 5;
+	case 'X':
+		return 10;
+	case 'L':
+		return 50;
+	case 'C':
+		return 100;
+	case 'D':
+		return 500;
+	case 'M':
+		return 1000;
+	default :
+		return 0;
+	}
+}
+
+int romanToInt(char* s) {
+	int result = getInt(s[0]);
+	for (int i = 1; i < (int)strlen(s); i++)
+	{
+		int j = getInt(s[i - 1]);
+		int k = getInt(s[i]);
+		if (j < k)
+		{
+			result = result + k - j * 2;
+		}
+		else {
+			result = result + k;
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	char *s = "MCMXCIV";
+	int result = romanToInt(s);
+	printf("%d\n", result);
+	return 0;
+}
