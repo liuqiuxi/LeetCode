18779caa8058ee561e53019ba53197858aec07a3
liuqiuxi <35030517+liuqiuxi@users.noreply.github.com>
2018-09-04 10:27:39 +0800

    Add files via upload

---
+++ LeetCode20 Valid Parentheses.c
@@ -0,0 +1,59 @@
+﻿#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+bool isValid(char* s) {
+	char *result = (char*)malloc(sizeof(char) * (int)strlen(s));
+	int top = -1;
+	int len = 0;
+	while (s[len])
+	{
+		if (s[len] == ')')
+		{
+			if (top >= 0 && result[top] == '(')
+			{
+				top--;
+			}
+			else {
+				return false;
+			}
+		}
+		else if (s[len] == ']')
+		{
+			if (top >= 0 && result[top] == '[')
+			{
+				top--;
+			}
+			else {
+				return false;
+			}
+		}
+		else if (s[len] == '}')
+		{
+			if (top >= 0 && result[top] == '{')
+			{
+				top--;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			top = top + 1;
+			result[top] = s[len];
+		}
+		len++;
+	}
+	free(result);
+	return top == -1;
+}
+
+int main()
+{
+	char *s = "[({(())}[()])]";
+	bool i = isValid(s);
+	printf("%d\n", i);
+	return 0;
+}
