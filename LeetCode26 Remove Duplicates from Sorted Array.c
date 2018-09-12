706f6094c61a34632f466651d27964aadd130d1b
liuqiuxi <35030517+liuqiuxi@users.noreply.github.com>
2018-09-12 13:59:18 +0800

    Add files via upload

---
+++ LeetCode26 Remove Duplicates from Sorted Array.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize) {
+	if (numsSize == 0) return 0;
+	int result = 0;
+	for (int i = 0; i < numsSize; i++)
+	{
+		if (nums[i] != nums[result])
+		{
+			result++;
+			nums[result] = nums[i];
+		}
+	}
+	return ++result;
+}
+
+int main()
+{
+	int nums[] = { 1,1,2,2,3,5,9,9 };
+	int numsSize = removeDuplicates(nums, sizeof(nums) / sizeof(nums[0]));
+	for (int i = 0; i < numsSize; i++) printf("%d", nums[i]);
+	return 0;
+}
