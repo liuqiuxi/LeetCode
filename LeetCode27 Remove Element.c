a0c337e051886697875c20c0a9f67e8a6ce76436
liuqiuxi <35030517+liuqiuxi@users.noreply.github.com>
2018-09-13 09:57:46 +0800

    Add files via upload

---
+++ LeetCode27 Remove Element.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val) {
+	int result = 0;
+	for (int i = 0; i < numsSize; i++)
+	{
+		if (nums[i] != val)
+		{
+			nums[result] = nums[i];
+			result++;
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	int nums[] = { 3,2,2,3 };
+	int len = removeElement(nums, sizeof(nums) / sizeof(nums[0]), 2);
+	for (int i = 0; i < len; i++) printf("%d", nums[i]);
+	return 0;
+}
