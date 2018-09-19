dbc48105d99d1463f09cb549c6d0713d49b27dca
liuqiuxi <1192448217@qq.com>
2018-09-19 13:38:30 +0800

    Add files via upload

---
+++ LeetCode35 Search Insert Position.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int searchInsert(int* nums, int numsSize, int target) 
+{
+	if (nums[0] > target) return 0;
+	if (nums[numsSize - 1] < target) return numsSize;
+	for (int i = 0; i < numsSize; i++) if ((nums[i] == target) || (nums[i - 1] < target && nums[i] > target)) return i;
+	return NULL;
+}
+
+int main()
+{
+	int nums[] = {1,3,5,6};
+	int target = 5;
+	int result = searchInsert(nums, sizeof(nums) / sizeof(nums[0]), target);
+	printf("the result is %d", result);
+	return 0;
+}
