9703450a3dc680bd28265ef81942cbbc68b5fad8
liuqiuxi <1192448217@qq.com>
2018-10-17 14:04:31 +0800

    Add files via upload

---
+++ LeetCode44 Wildcard Matching.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+bool isMatch(char* s, char* p) 
+{
+	char *scur = s, *pcur = p, *sstar = NULL, *pstar = NULL;
+	while (*scur)
+	{
+		if (*scur == *pcur || *pcur == '?'){
+			scur++;
+			pcur++;
+		}else if (*pcur == '*') {
+			pstar = pcur++;
+			sstar = scur;
+		}else if (pstar) {
+			pcur = pstar + 1;
+			scur = sstar++;
+		}else {
+			return false;
+		}
+	}
+	while (*pcur == '*') pcur++;
+	return !*pcur;
+}
+
+int main()
+{
+	char s[] = "adceb";
+	char p[] = "*a*b";
+	bool result = isMatch(s, p);
+	printf("The result is %d", result);
+	return 0;
+}
