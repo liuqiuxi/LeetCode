d73bae7abe3bccb02b6e2814e60ee0a232a43d82
liuqiuxi <1192448217@qq.com>
2018-10-23 14:13:22 +0800

    Add files via upload

---
+++ LeetCode52 N-Queens II.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+bool isValid(int row, int *arrays)
+{
+	for (int i = 0; i < row; i++) if (arrays[row] == arrays[i] || abs(arrays[row] - arrays[i]) == row - i) return false;
+	return true;
+}
+
+void nqueens(int row, int n, int *arrays, int *returnSize)
+{
+	if (row == n) (*returnSize)++;
+	for (int i = 0; i < n; i++)
+	{
+		arrays[row] = i;
+		if (isValid(row, arrays)) nqueens(row + 1, n, arrays, returnSize);
+	}
+}
+
+int totalNQueens(int n)
+{
+	if (n < 1 ) return 0;
+	int *arrays = (int*)calloc(n + 1, sizeof(int));
+	int returnSize = 0;
+	nqueens(0, n, arrays, &returnSize);
+	free(arrays);
+	return returnSize;
+}
+
+int main()
+{
+	int n = 4;
+	int result = totalNQueens(n);
+	printf("The result is %d", result);
+	return 0;
+}
