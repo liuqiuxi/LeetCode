b692d379168e87b75902c3d65684d70c759fba50
liuqiuxi <1192448217@qq.com>
2018-10-24 10:18:57 +0800

    Add files via upload

---
+++ LeetCode53 Maximum Subarray.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+#define MIN -2147483647 - 1
+
+int maxSubArray(int* nums, int numsSize) 
+{
+	int result = MIN, curSum = 0;
+	for (int i = 0; i < numsSize; i++)
+	{
+		curSum = curSum + nums[i] > nums[i] ? curSum + nums[i] : nums[i];
+		result = result > curSum ? result : curSum;
+	}
+	return result;
+}
+
+int main()
+{
+	int nums[] = { -2,1,-3,4,-1,2,1,-5,4 };
+	int result = maxSubArray(nums, sizeof(nums) / sizeof(nums[0]));
+	printf("The result is %d", result);
+	return 0;
+}
