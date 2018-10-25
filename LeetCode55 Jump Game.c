ae611899a0e856e2e4e02657fafbfd0953384c2a
liuqiuxi <1192448217@qq.com>
2018-10-25 10:04:50 +0800

    Add files via upload

---
+++ LeetCode55 Jump Game.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+
+bool canJump(int* nums, int numsSize) 
+{
+	int arrive = 0;
+	for (int i = 0; i < numsSize; i++)
+	{
+		if (i > arrive || arrive >= numsSize - 1) break;
+		arrive = arrive > i + nums[i] ? arrive : i + nums[i];
+	}
+	return arrive >= numsSize - 1;
+}
+
+int main()
+{
+	int nums[] = { 3,2,1,0,4 };
+	bool result = canJump(nums, (int)(sizeof(nums) / sizeof(nums[0])));
+	printf("The result is %d", result);
+	return 0;
+}
