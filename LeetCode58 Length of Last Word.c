8e35c3879091b9d73bb1fb8a8c222553bc56cbb3
liuqiuxi <1192448217@qq.com>
2018-10-26 13:48:01 +0800

    Add files via upload

---
+++ LeetCode58 Length of Last Word.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int lengthOfLastWord(char* s) 
+{
+	int len = strlen(s), result = 0, lastResult = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (s[i] == ' ' && s[i - 1] != ' ') lastResult = result;
+		if (s[i] == ' ')
+		{
+			result = 0;
+			continue;
+		}
+		result++;
+	}
+	if (s[len - 1] == ' ') result = lastResult;
+	return result;
+}
+
+int main()
+{
+	char s[] = { "   aa   b " };
+	int result = lengthOfLastWord(s);
+	printf("The result is %d", result);
+	return 0;
+}
