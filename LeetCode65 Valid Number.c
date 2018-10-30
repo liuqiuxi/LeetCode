33aa12a0aa5664d8296e3bcadd809bf3d5ca893c
liuqiuxi <1192448217@qq.com>
2018-10-30 14:44:13 +0800

    Add files via upload

---
+++ LeetCode65 Valid Number.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+void skipBlank(char **s)
+{
+	while (**s == ' ') (*s)++;
+}
+
+int digitNumber(char **s, bool allowSign)
+{
+	int number = 0;
+	if (allowSign == true) if (**s == '+' || **s == '-') (*s)++;
+	while (**s >= '0' && **s <= '9')
+	{
+		(*s)++; number++;
+	}
+	return number;
+}
+
+bool isNumber(char* s)
+{
+	skipBlank(&s);
+	int number = 0, floatnumber = 0;
+	number = digitNumber(&s, true);
+	if (*s == '.')
+	{
+		s++;
+		floatnumber = digitNumber(&s, false);
+	}
+	if ((*s == 'e' || *s == 'E') && (number + floatnumber > 0))
+	{
+		s++;
+		if (!(number = digitNumber(&s, true))) return false;
+	}
+	skipBlank(&s);
+	return (number + floatnumber) && !*s;
+}
+
+int main()
+{
+	char *s = " 2e10 ";
+	bool result = isNumber(s);
+	printf("The result is %d", result);
+	return 0;
+}
