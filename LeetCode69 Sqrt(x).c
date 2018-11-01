8a8618d9194eda5b7e9a8bf08fe096564d1185ba
liuqiuxi <1192448217@qq.com>
2018-11-01 13:37:37 +0800

    Add files via upload

---
+++ LeetCode69 Sqrt(x).c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int mySqrt(int x)
+{
+	double n = (double)x;
+	int result = (int)sqrt(n);
+	return result;
+}
+
+int main()
+{
+	int x = 8;
+	int result = mySqrt(x);
+	printf("The result is %d", result);
+	return 0;
+}
